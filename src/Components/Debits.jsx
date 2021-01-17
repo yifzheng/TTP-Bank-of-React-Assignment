158fd9d4b8265b8a30dc155b43ca6a5064892386
yifzheng <70163313+yifzheng@users.noreply.github.com>
2021-01-17 11:03:22 -0500

    added comments to debits.jsx

--- src/Components/Debits.jsx
+++ src/Components/Debits.jsx
@@ -9,20 +9,20 @@ class Debits extends Component {
         super(props);
 
         this.state = {
-            display: false,
-            displayDebit: false,
-            addDebit: false,
+            display: false, // to handle display of accountBalance
+            displayDebit: false, // to handle the display of the debit components
+            addDebit: false, // to handle display of form to add another debit component
             totalBalance: this.props.totalAmount,
             creditAmount: this.props.creditAmount,
             debitAmount: this.props.amount,
             debit: this.props.debits
         }
-        this.formDescription = React.createRef();
-        this.formAmount = React.createRef();
-        this.formDate = React.createRef();
+        this.formDescription = React.createRef(); // reference for form
+        this.formAmount = React.createRef(); // reference for form
+        this.formDate = React.createRef(); // reference for form
     }
 
-
+    // function used in prior code
     setBalance = () => {
         const num = this.props.creditAmount;
         return (
@@ -31,42 +31,46 @@ class Debits extends Component {
             })
         )
     }
-
+    // the submit function called when click submit on form
     handleSubmit = () => {
-        Number(this.formAmount.current.value);
+        Number(this.formAmount.current.value); // makes the form amount a number
         let num = parseFloat(this.formAmount.current.value)
+        // create an object storing necessary information toi update the state of debit
         let obj = {
             id: this.formDescription.current.value + this.formAmount.current.value + this.formDate.current.value + Math.random() * 100,
             description: this.formDescription.current.value,
             amount: num,
             date: this.formDate.current.value
         }
-        let arr = this.props.debits;
-        arr.push(obj);
-        const total = parseFloat(this.state.debitAmount + num);
+        let arr = this.props.debits; // assign an variable equal to debit state
+        arr.push(obj); // push obj made previously to array
+        const total = parseFloat(this.state.debitAmount + num); // variable used to store new amount of debit $
+        // update the state of debit with new arr, debitAmount with new total, and addDebit to false
         this.setState({
             debitAmount: total,
             debit: arr,
             addDebit: false
         })
+        // invoke the callback function to pass the debit $ amount to the parent component
         this.props.debitCallBack(num);
     }
 
-
+    // viewBalance function updates state allowing the balance component to show and remove the form display if it is currently showing
     viewBalance = () => {
         this.setState({
             display: !this.state.display,
             addDebit: false
         })
     }
-
+    // handle cancel which removes display of form
     handleCancel = () => {
         this.setState({
             addDebit: false
         })
     }
-
+    // render()
     render() {
+        // map through the debit state property and for every object in the arr, make a debit component with the necessary information
         let displayDebit = <div id="debit-cards">
             {
                 this.state.debit.map((item) => {
@@ -78,17 +82,22 @@ class Debits extends Component {
         </div>
         return (
             <div id="debits-page">
+                {// if addDebit and Display of balance is false, display the buttons}
                 {!this.state.addDebit && !this.state.display && <div className="btn">
+                        {//link/route to homepage}
                     <Link to="/" style={{ textDecoration: 'none' }}><button className="return-home" >Home Page</button></Link>
+                            {// display the debit components}
                     <button className="return-home" style={{ textDecoration: 'none' }} onClick={e => this.setState({ displayDebit: !this.state.displayDebit })}>Display Debit</button>
                     <button className="balanceBtn" onClick={this.viewBalance}>View Balance</button>
                     <button className="addBtn" onClick={e => { this.setState({ addDebit: !this.state.addDebit, display: false }) }}>Add Debit</button>
                 </div>}
+                            {// if display is true, render the account balance}
                 {this.state.display &&
                     <div id = "display-balance">
                         <h1>Account Balance: ${this.props.totalAmount.toFixed(2)}</h1>
                         <button className="return-debit" onClick={() => this.setState({display : false})}>Back</button>
                     </div>}
+                                {// if addDebit is true render the form to add debit component to debit state}
                 {this.state.addDebit &&
                         (<div id="debit-form">
                             <form>
@@ -114,6 +123,7 @@ class Debits extends Component {
                 <h1>{this.props.message}</h1>
                 <br></br>
                 <br></br>
+                                    {// if displayDebit is true, render all debit components}
                 {this.state.displayDebit && displayDebit}
             </div>
 
